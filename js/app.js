af53a58683297c2b4828a33e28b0e34af8176956
lucastiago88 <lucastiago_silva@hotmail.com>
2014-07-25 17:36:08 -0300

    add propriedades para login

--- js/app.js
+++ js/app.js
@@ -9,30 +9,5 @@ angular.module('joinapp', ['ionic'])
   ];
 });
 
-angular.module('ionicApp', ['ionic'])
+angular.module('joinapp', ['ionic'])
 
-.controller('MainCtrl', function($scope) {
-
-  $scope.clientSideList = [
-    { text: "Backbone", value: "bb" },
-    { text: "Angular", value: "ng" },
-    { text: "Ember", value: "em" },
-    { text: "Knockout", value: "ko" }
-  ];
-
-  $scope.serverSideList = [
-    { text: "Go", value: "go" },
-    { text: "Python", value: "py" },
-    { text: "Ruby", value: "rb" },
-    { text: "Java", value: "jv" }
-  ];
-  
-  $scope.data = {
-    clientSide: 'ng'
-  };
-  
-  $scope.serverSideChange = function(item) {
-    console.log("Selected Serverside, text:", item.text, "value:", item.value);
-  };
-  
-});
