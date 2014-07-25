e6b8751808cbc7ea28f4494e7a6873f30ccd0b7c
lucastiago88 <lucastiago_silva@hotmail.com>
2014-07-25 09:14:10 -0300

    subindo arquivos

---
+++ js/app.js
@@ -0,0 +1,38 @@
+angular.module('joinapp', ['ionic'])
+
+.controller('JoinCtrl', function($scope) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+});
+
+angular.module('ionicApp', ['ionic'])
+
+.controller('MainCtrl', function($scope) {
+
+  $scope.clientSideList = [
+    { text: "Backbone", value: "bb" },
+    { text: "Angular", value: "ng" },
+    { text: "Ember", value: "em" },
+    { text: "Knockout", value: "ko" }
+  ];
+
+  $scope.serverSideList = [
+    { text: "Go", value: "go" },
+    { text: "Python", value: "py" },
+    { text: "Ruby", value: "rb" },
+    { text: "Java", value: "jv" }
+  ];
+  
+  $scope.data = {
+    clientSide: 'ng'
+  };
+  
+  $scope.serverSideChange = function(item) {
+    console.log("Selected Serverside, text:", item.text, "value:", item.value);
+  };
+  
+});
